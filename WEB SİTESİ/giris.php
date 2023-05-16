574d1ad4510f610065f4326570cd47d0925049a9
suleylmaz <121484318+suleylmaz@users.noreply.github.com>
2023-05-16 21:37:16 +0300

    Add files via upload

---
+++ WEB SİTESİ/giris.php
@@ -0,0 +1,14 @@
+<?php
+
+    if(isset($_POST['email'],$_POST['sifre'])&&($_POST['email']=='B221210033@sakarya.edu.tr')
+     &&($_POST['sifre']=='B221210033'))
+    {
+        echo("HOŞGELDİN B221210033 :)");
+    }
+    else
+    {   
+        echo("HATALI GİRİŞ YAPTINIZ!!");
+        echo"<p> <a href='giriş.html'>&lt;GERİ DÖN&gt;</a></p>";
+    }
+   
+?>
